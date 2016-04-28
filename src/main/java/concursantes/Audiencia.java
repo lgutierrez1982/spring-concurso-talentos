9a439c4660406fb7b81b17e2f0202cca9fb13495
lgutierrez <lgutierrez1982@gmail.com>
2016-04-28 18:43:22 -0400

    ejemplo spring con aop uso de anotaciones

--- src/main/java/concursantes/Audiencia.java
+++ src/main/java/concursantes/Audiencia.java
@@ -1,21 +1,28 @@
 package concursantes;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect // aspecto aop
 public class Audiencia {
+	@Pointcut("execution(* concursantes.Concursante.ejecutar(..))")// cual es el metodo a monitorear
+	public void ejecutarShow() {//cuando se agrega anotacion @Pointcut de java se debe especificar un metodo 
+	}							//que se va a ejecutar cuando se intersecte el pointcut
+
+	@Around("ejecutarShow()")//cuando se define el advice se debe indicar cual es el metodo que funciono en la definicion del pointcut 
 	public void monitorearShow(ProceedingJoinPoint joinpoint) {
 		try {
 			System.out.println("El show está por comenzar, favor de tomar asiento...");
-
 			System.out.println("Favor de apagar celulares...");
 			// Anotamos la hora de inicio
 			long horaInicio = System.currentTimeMillis();
 			// Se llama al método de negocio (método objetivo)
 			joinpoint.proceed();
-			Thread.sleep(1000);
-			// 1 segundo
+			Thread.sleep(1000); // 1 segundo
 			// Este delay en milisegundos es opcional y se puede poner en los
 			// métodos
 			// de negocio para simular la duración del método
